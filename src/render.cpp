7c3fd2f1aee415f56bb3d8612526ae35a7d06845
ununseptium <54485315+ununseptium@users.noreply.github.com>
2021-01-23 00:49:18 +0300

    Add render file

---
+++ src/render.cpp
@@ -0,0 +1,7 @@
+#include <render.h>
+
+DWORD test_thread_fnc(void* param){
+	while(1){
+	}
+	return 0;
+}
---
+++ src/render.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <windows.h>
+
+DWORD test_thread_fnc(void* param);
