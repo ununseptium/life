a00f8dcb9c35cd1436ec0e0d4bb67932857d3ad2
ununseptium <54485315+ununseptium@users.noreply.github.com>
2021-01-24 02:14:01 +0300

    Add render_info struct in render.h

--- src/render.h
+++ src/render.h
@@ -1,4 +1,15 @@
 #include <stdio.h>
 #include <windows.h>
 
+#ifndef _RENDER_INFO
+#define _RENDER_INFO
+struct render_info{
+	uint8_t cells_count_width;
+	uint8_t cells_count_height;
+	uint8_t *cells_array;
+	void *grid;
+	void (*update_grid_fnc)(void* grid, uint8_t *cells_array);
+}
+#endif
+
 DWORD test_thread_fnc(void* param);
