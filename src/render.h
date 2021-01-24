b5eb570292bcb70226155dc8699979a4c951936c
ununseptium <54485315+ununseptium@users.noreply.github.com>
2021-01-24 23:27:55 +0300

    Add neighbors_cells struct to render.h

--- src/render.h
+++ src/render.h
@@ -13,4 +13,19 @@ struct render_info{
 }
 #endif
 
+#ifndef _NEIGHBORS_CELLS
+#define _NEIGHBORS_CELLS
+struct neighbors_cells{
+	// start from top-left neighbor's cell; clockwise
+	uint8_t neigh0;
+	uint8_t neigh1;
+	uint8_t neigh2;
+	uint8_t neigh3;
+	uint8_t neigh4;
+	uint8_t neigh5;
+	uint8_t neigh6;
+	uint8_t neigh7;
+}
+#endif
+
 DWORD render_cycle(struct render_info* ri);
