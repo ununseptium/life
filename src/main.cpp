83193060f7ba5a27e8093995b1268ff2fc3ebab3
ununseptium <54485315+ununseptium@users.noreply.github.com>
2021-02-13 18:52:01 +0300

    Add command line args parser use to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#include <stdio.h>
 #include <life_gui.h>
+#include <comand_line_parser.h>
 
 int main (int argc, char** argv){
+	uint8_t width_cl = 0;
+	uint8_t height_cl = 0;
+	uint16_t time_cl = 0;
+	if (cl_parsing(argc, argv, &width_cl, &height_cl, &time_cl) != 0){
+		puts("Command line arguments are incorrect");
+		return 1;
+	}
+
 	event_loop el = init_event_loop(argv);
 	create_gui(10, 10, 0);
 	start_event_loop(el);
