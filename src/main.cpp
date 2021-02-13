ae4f5c24976b892ca16ea44707ea676ffec7a0be
ununseptium <54485315+ununseptium@users.noreply.github.com>
2021-02-13 18:58:02 +0300

    Add config parser use to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <life_gui.h>
+#include <config_parser.h>
 #include <comand_line_parser.h>
 
 int main (int argc, char** argv){
@@ -11,6 +12,18 @@ int main (int argc, char** argv){
 		return 1;
 	}
 
+	uint8_t width_conf;
+	uint8_t height_conf;
+	uint16_t time_conf;
+	uint32_t err_val = conf_parsing(&width_conf, &height_conf, &time_conf);
+	if (err_val != 0){
+		if (err_val == 1)
+			puts("Configuration file does not exist");
+		if (err_val == 2)
+			puts("Configuration file is corrupt");
+		return 1;
+	}
+	
 	event_loop el = init_event_loop(argv);
 	create_gui(10, 10, 0);
 	start_event_loop(el);
