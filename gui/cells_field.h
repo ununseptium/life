57fbf50a744b1e52b33736ee838a1ce1a255c16f
ununseptium <54485315+ununseptium@users.noreply.github.com>
2021-02-21 01:07:49 +0300

    Change return value of get_cell in cells_field
    It was changed cause of Qt logic.

--- gui/cells_field.h
+++ gui/cells_field.h
@@ -21,7 +21,7 @@ class cells_field: public QGraphicsView{
 				uint8_t cells_count_width, uint8_t cells_count_height, QGraphicsPixmapItem *start_border, 
 				QGraphicsPixmapItem *left_border, QGraphicsPixmapItem *up_border		
 		);
-		cell get_cell(uint8_t w, uint8_t h);
+		cell* get_cell(uint8_t w, uint8_t h);
 		void set_cell(cell c, uint8_t w, uint8_t h);
 };
 
